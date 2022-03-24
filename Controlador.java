80c800a088761314a2d2e2c83d92755d05f213b6
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-03-23 23:31:42 -0600

    Implementación de las variables listas
    Edición para poder crear las variables que sean listas.

--- Controlador.java
+++ Controlador.java
@@ -4,24 +4,64 @@ import java.util.HashMap;
 public class Controlador
 {
     private HashMap<String, Integer> variables = new HashMap<>();
+    private HashMap<String, String> listas = new HashMap<>();
 
     public void setq(String instruccion)
     {
-        /*StringBuilder editado = new StringBuilder(instruccion);
+        instruccion = instruccion.trim().replaceAll("\\s+", " ");
+        StringBuilder editado = new StringBuilder(instruccion);
+
+        editado.deleteCharAt(0);
+        editado.deleteCharAt(editado.length() - 1);
+
+        String lista = "";
+        int inicio = 0;
+        int fin = 0;
         for(int i = 0; i < editado.length(); i++)
         {
             String caracter = String.valueOf(editado.charAt(i));
-            if(caracter.equals("(") || caracter.equals(")")) editado.deleteCharAt(i);
+            if(caracter.equals("(")) inicio = i;
+            if(caracter.equals(")")) fin = i;
         }
+
+        boolean esLista = false;
+
+        if(inicio != 0 && fin != 0)
+        {
+            esLista = true;
+            for(int x = inicio; x <= fin; x++)
+            {
+                lista += String.valueOf(editado.charAt(x));
+            }
+
+            StringBuilder listaE = new StringBuilder(lista);
+            for(int i = 0; i < listaE.length(); i++)
+            {
+                String caracter = String.valueOf(listaE.charAt(i));
+                if(caracter.equals("(") || caracter.equals(")")) listaE.deleteCharAt(i); //Se eliminan los parentesis para poder hacer la limpieza de espacios en blanco innecesarios
+            }
+            lista = new String(listaE).trim().replaceAll("\\s+", " ");
+            lista = "(" + lista + ")";
+
+        }
+
         int s = editado.indexOf("s");
         int q = editado.indexOf("q");
         if((q - s) == 3) editado.delete(s, q + 1);
         instruccion = new String(editado).trim();
-        instruccion = instruccion.replaceAll("\\s+", " ");*/
+        instruccion = instruccion.replaceAll("\\s+", " ");
 
         instruccion = this.limpiar(instruccion, "s", "q", 3);
         String[] split = instruccion.split(" ");
-        this.variables.put(split[0], Integer.parseInt(split[split.length - 1]));
+
+        if(esLista)
+        {
+            this.listas.put(split[0], lista);
+        }
+        else
+        {
+            this.variables.put(split[0], Integer.parseInt(split[split.length - 1]));
+        }
     }
 
     private String limpiar(String instruccion, String letraInicio, String letraFinal, int largo)
@@ -42,7 +82,7 @@ public class Controlador
 
     public String imprimir()
     {
-        return "\n" + String.valueOf(this.variables);
+        return "\n" + String.valueOf(this.variables) + "\n" + String.valueOf(this.listas);
     }
 
     public String operaciones()
--- Principal.java
+++ Principal.java
@@ -21,27 +21,47 @@ public class Principal
         instruccion = scanner.nextLine();
         controlador.setq(instruccion);
         
+        System.out.println("\nIngrese la linea: ");
+        instruccion = scanner.nextLine();
+        controlador.setq(instruccion);
         
         
         System.out.println(controlador.imprimir());
         
         
-        System.out.println(controlador.operaciones());
+        //System.out.println(controlador.operaciones());
         
 
-        
-        /*----------------------------------------------------
+        /*--------------------------------------------------
+        instruccion = instruccion.trim().replaceAll("\\s+", " ");
         StringBuilder editado = new StringBuilder(instruccion);
-        for(int i = 0; i < editado.length(); i++)
-        {
-            String caracter = String.valueOf(editado.charAt(i));
-            if(caracter.equals("(") || caracter.equals(")")) editado.deleteCharAt(i);
-        }
-        //editado.deleteCharAt(0);
-        //editado.deleteCharAt(editado.length() - 1);
+        
+        System.out.println("\nSin espacios: '" + instruccion + "'");
+        
+        editado.deleteCharAt(0);
+        editado.deleteCharAt(editado.length() - 1);
         
         System.out.println("\nEditado: '" + editado + "'");
         
+        String lista = "";
+        int inicio = 0;
+        int fin = 0;
+        for(int i = 0; i < editado.length(); i++)
+        {
+            String caracter = String.valueOf(editado.charAt(i));
+            if(caracter.equals("(")) inicio = i;
+            if(caracter.equals(")")) fin = i;
+        }
+
+        System.out.println("\nInicio: '" + inicio + "'");
+        System.out.println("\nFin: '" + fin + "'");
+
+        for(int x = inicio; x <= fin; x++)
+        {
+            lista += String.valueOf(editado.charAt(x));
+        }
+
+        System.out.println("\nLista: '" + lista + "'");
         
         int s = editado.indexOf("s");
         int q = editado.indexOf("q");
@@ -57,7 +77,7 @@ public class Principal
         String[] split = instruccion.split(" ");
 
         System.out.println("\nNombre: '" + split[0] + "' Valor: '" + split[1] + "'" + " Cantidad: " + split.length);
-        ----------------------------------------------------------------*/
+        -------------------------------------------------------*/
 
 
        
