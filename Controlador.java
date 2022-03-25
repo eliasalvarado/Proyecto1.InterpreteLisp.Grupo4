d1ac9132cba9686f552bda0c8a0daa52e0d5c8f9
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-03-24 23:38:55 -0600

    Update Controlador.java
    Implementacion de Quote y COND

--- Controlador.java
+++ Controlador.java
@@ -407,4 +407,94 @@ public class Controlador
             return true;
         }
     }
+    public String Quote(String arg){
+        return arg;                 
+    }
+    public String Cond(String args){
+        String[] lineas = args.split("\\n");
+		String txt="";
+		String operador="";
+		String valor1 = "";
+		String valor2 = "";
+		String accion = "";
+		String linea="";
+		String expresion="";
+        for (int i =0;i<lineas.length;i++) {//ciclo que recorre todas las líneas
+			expresion ="";			
+			operador="";
+			valor1="";
+			valor2="";
+			accion="";
+			linea = lineas[i];
+            
+			for (int j = 0; j<linea.length();j++) {// quita parentesis de la expresion
+				if (String.valueOf(linea.charAt(j)).equals("(")||String.valueOf(linea.charAt(j)).equals(")")) {
+					//no pasa nada
+				}else expresion = expresion +linea.charAt(j);
+			}
+
+			operador = expresion.substring(0, 1).trim();
+			
+			expresion = expresion.substring(1).trim(); //quita operador de la expresion 
+			boolean bandera = false;
+
+			for (int x = 0;x<expresion.length();x++) {	//ciclo para encontrar primer valor			
+				if(String.valueOf(expresion.charAt(x)).equals(" ")) {
+					bandera = true;//encontro un espacio ya no concatena más al valor
+				}else {
+					if (bandera == false) {
+						valor1 = valor1+=expresion.charAt(x);
+					}
+				}
+			}
+
+			valor1 = valor1.trim();
+			expresion = expresion.substring(valor1.length()).trim(); //quita valor1 de la expresion
+			boolean bandera2 = false;
+			
+            for (int w = 0;w<expresion.length();w++) {	//ciclo para encontrar primer valor			
+				if(String.valueOf(expresion.charAt(w)).equals(" ")) {
+					bandera2 = true;//encontro un espacio ya no concatena más al valor
+				}else {
+					if (bandera2 == false) {
+						valor2 = valor2+=expresion.charAt(w);
+					}
+				}
+			}
+            
+			valor2.trim();
+			expresion = expresion.substring(valor2.length()).trim(); //quita valor2 de la expresion (dejando solo la accion)
+			accion = expresion;
+			
+			//--------------------------------ya estan todos separados------------------------------------------
+			
+			//-------------------------------comienza la evaluación---------------------------------------------
+			int val1 = variables.get(valor1); //llamar al Hash con nombre de la variable
+			int val2 = Integer.parseInt(valor2);
+			switch(operador) {
+			case "=":				
+				if (val1 == val2) {
+					if(txt.equals("")) {
+						txt=accion;
+					}					
+				}
+				break;
+			case ">":
+				if (val1 > val2) {
+					if(txt.equals("")) {
+						txt=accion;
+					}
+				}
+				break;
+			case "<":
+				if (val1 < val2) {
+					if(txt.equals("")) {
+						txt=accion;
+					}
+				}
+				break;
+			}
+		}
+        return txt;
+    }
 }
